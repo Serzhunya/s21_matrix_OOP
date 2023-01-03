3b56ecd31b3015f23670f4156c0cd64e3455588f
Sandslash Dorla <sandslas@ga-r5.msk.21-school.ru>
2023-01-03 19:53:15 +0300

    add matrix_oop.h

--- src/s21_matrix_oop.h
+++ src/s21_matrix_oop.h
@@ -1,5 +1,33 @@
 #ifndef __S21MATRIXOOP_H__
 #define __S21MATRIXOOP_H__
-#include <iostream> // директива препроцессора
+#include <iostream>  // директива препроцессора
+
+class S21Matrix {
+ private:
+  // Attributes
+  int rows_;
+  int cols_;
+  double** matrix_;  // Pointer to the memory where the matrix is allocated
+
+ public:
+  S21Matrix();   // Default constructor
+  ~S21Matrix();  // Destructor
+  S21Matrix(int rows, int cols) {
+    rows_ = rows;
+    cols_ = cols;
+  }
+  S21Matrix(const S21Matrix& other) {}
+
+  S21Matrix(S21Matrix&& other) {}
+  bool EqMatrix(const S21Matrix& other);
+  void SumMatrix(const S21Matrix& other);
+  void SubMatrix(const S21Matrix& other);
+  void MulNumber(const double num);
+  void MulMatrix(const S21Matrix& other);
+  S21Matrix Transpose();
+  S21Matrix CalcComplements();
+  double Determinant();
+  S21Matrix InverseMatrix();
+};
 
 #endif
